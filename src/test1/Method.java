e59d6a49a086c6cac003b2999b6e7b38dd5ab8a8
Mohan <mohans3033@gmail.com>
2023-09-26 22:33:59 +0530

    method

---
+++ src/test1/Method.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("out");
+		System.out.println("in");
+
+	}
+
+}
